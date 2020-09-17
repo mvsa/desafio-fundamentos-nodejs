967effa21f2704a249eed1abb8c53a84f1712bb2
Marcos Vinícios <mvsa@cin.ufpe.br>
2020-09-17 17:17:41 -0300

    solution

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,38 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions
+      .filter(transaction => transaction.type === 'income')
+      .reduce((sum, element) => sum + element.value, 0);
+
+    const outcome = this.transactions
+      .filter(transaction => transaction.type === 'outcome')
+      .reduce((sum, element) => sum + element.value, 0);
+
+    const total = income - outcome;
+    const balance = { income, outcome, total };
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public checkPositiveBalance(value: number): boolean {
+    const { total } = this.getBalance();
+
+    if (value > total) {
+      return false;
+    }
+    return true;
+  }
+
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
